40f4bb1d04eca48de2fa47c9a85c2ee79838d248
Tanishq Jain <tan@Tanishqs-MacBook-Pro.local>
2022-04-29 03:11:27 -0400

    dwqdqw

--- twix/src/main.rs
+++ twix/src/main.rs
@@ -1,16 +1,14 @@
-#![allow(unused)]
-
 mod rules;
 
 use std::sync::Arc;
 use std::io::{self, Error, ErrorKind};
-use std::os::unix::thread;
+
 use std::time::Duration;
 
 use mars::Mars;
-
+use skittles::SkittlesClient;
 use clap::Parser;
-use sysinfo::{System, SystemExt, ProcessorExt, NetworkExt};
+use sysinfo::{System, SystemExt, NetworkExt};
 use tokio::net::TcpStream;
 use tokio::sync::{Mutex, MutexGuard};
 
@@ -19,55 +17,67 @@ use crate::rules::Rules;
 #[derive(Parser, Debug)]
 #[clap(name="Twix", version = "1.0")]
 struct Args {
-    #[clap(short, long)]
+    #[clap(long, default_value = "TWIX-1")]
+    name: String,
+
+    #[clap(short, long, default_value_t = 10)]
     interval: u64,
 
-    #[clap(short, long)]
+    #[clap(short, long, default_value_t = 4545)]
     port: u16,
 
-    #[clap(short, long)]
-    rules: String,
-
-    #[clap(long, default_value_t = 99.9)]
+    #[clap(short, long, default_value_t = 1.0)]
     ram: f32,
 
-    #[clap(long, default_value_t = 99.9)]
+    #[clap(short, long, default_value_t = 1.0)]
     memory: f32,
 
-    #[clap(long, default_value_t = 10240)]
-    network: usize
+    #[clap(short, long, default_value_t = 1)]
+    network: usize,
+
+    #[clap(long)]
+    log: bool,
+
+    #[clap(long)]
+    log_ip: String
 }
 
 #[tokio::main(flavor = "current_thread")] // single threaded
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
+    let log = SkittlesClient::new(args.name, args.log_ip, args.log);
+
     let mut mars = Mars::new();
-    mars.add_topic("ram,memory,network");
-    let mut mars = Arc::new(Mutex::new(Mars::new()));
+    mars.add_topic("ram,memory,network").await;
+    let mars = Arc::new(Mutex::new(Mars::new()));
 
     let mars_sys = Arc::clone(&mars);
+    let sys_log = log.clone();
+
     let system = tokio::spawn(async move {
         use tokio::net::TcpListener;
 
         let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await
                                     .expect("Could not start TCP Server");
 
+        sys_log.log(format!("Listening on port {}", args.port).as_str()).await;
+
         loop {
-            let (mut socket, _addr) = listener.accept().await.expect("Error while accepting socket");
+            let (socket, _addr) = listener.accept().await.expect("Error while accepting socket");
             {
                 let guard = mars_sys.lock().await;
 
-                process(guard, socket).await;    
+                let _ = process(guard, socket).await;    
             }
-            
         }
     });
 
     let mars_req = Arc::clone(&mars);
+    let req_log = log.clone();
     let req = tokio::spawn(async move {
         let mut s = System::new();
-        let mut r = Rules::new(args.ram, args.memory, args.network);
+        let r = Rules::new(args.ram, args.memory, args.network);
         loop {
             s.refresh_cpu();
             s.refresh_memory();
@@ -75,7 +85,7 @@ async fn main() -> std::io::Result<()> {
 
             let ram = s.available_memory();
             let mut network = 0;
-            s.networks().into_iter().map(|f| network += f.1.transmitted());
+            let _ = s.networks().into_iter().map(|f| network += f.1.transmitted());
 
             let (b1, b2, b3) = r.check(0.0, ram as f32, network as usize);
 
@@ -83,15 +93,18 @@ async fn main() -> std::io::Result<()> {
                 let mars = mars_req.lock().await;
 
                 if b1 {
-                    mars.send_topic_message("ram", b"Limit Crossed");
+                    req_log.log("RAM Limit Crossed").await;
+                    mars.send_topic_message("ram", b"Limit Crossed").await;
                 }
 
                 if b2 {
-                    mars.send_topic_message("memory", b"Limit Crossed");
+                    req_log.log("Storage Limit Crossed").await;
+                    mars.send_topic_message("memory", b"Limit Crossed").await;
                 }
 
                 if b3 {
-                    mars.send_topic_message("network", b"Limit Crossed");
+                    req_log.log("Bandwidth Limit Crossed").await;
+                    mars.send_topic_message("network", b"Limit Crossed").await;
                 }
             }
 
@@ -99,7 +112,7 @@ async fn main() -> std::io::Result<()> {
         }
     });
 
-    tokio::join!(system, req);
+    let _ = tokio::join!(system, req);
 
     Ok(())
 }
@@ -112,9 +125,9 @@ async fn process(mut mars: MutexGuard<'_, Mars>, s: TcpStream) -> io::Result<()>
 
     match String::from_utf8(buffer[0..n].to_vec()) {
         Ok(string) => {
-            mars.add_subscriber(s, &string);
+            mars.add_subscriber(s, &string).await;
         },
-        Err(e) => return io::Result::Err(Error::new(ErrorKind::InvalidData, "Invalid Data")),
+        Err(_e) => return io::Result::Err(Error::new(ErrorKind::InvalidData, "Invalid Data")),
     }
 
     Ok(())
