83bae26697691c33197c5f33c3d82a1f62cd206d
Tanishq Jain <tan@Tanishqs-MacBook-Pro.local>
2022-04-29 04:38:42 -0400

    mars log

--- mars/src/lib.rs
+++ mars/src/lib.rs
@@ -5,17 +5,20 @@ use std::collections::HashMap;
 use tokio::net::TcpStream;
 use tokio::sync::{mpsc, Mutex};
 
+use skittles::SkittlesClient;
+
 type Message = Vec<u8>;
 type Tx = mpsc::UnboundedSender<(String, Message)>;
 
 /// Pub/Sub
-pub struct Mars(Arc<Mutex<HashMap<String, Vec<(Tx, SocketAddr)>>>>);
+pub struct Mars(Arc<Mutex<HashMap<String, Vec<(Tx, SocketAddr)>>>>, SkittlesClient);
 
 impl Mars {
     /// Constructor
-    pub fn new() -> Self {
+    pub fn new(s: SkittlesClient) -> Self {
         Self {
-            0: Arc::new(Mutex::new(HashMap::new()))
+            0: Arc::new(Mutex::new(HashMap::new())),
+            1: s
         }
     }
 
@@ -93,6 +96,7 @@ impl Mars {
         for topic in topics {
             guard.insert(topic, vec![]);
         }
+        self.1.log(format!("Added topics: {}", s).as_str()).await;
     }
 
     /// Drop topics(comma separated)
@@ -103,6 +107,8 @@ impl Mars {
         for topic in topics {
             guard.remove(&topic);
         }
+
+        self.1.log(format!("Dropped topics: {}", s).as_str()).await;
     }
 
     /// Send a message to everyone listening on a particular topic(single topic)
@@ -112,6 +118,8 @@ impl Mars {
                 for (tx, _) in vt {
                     let _ = tx.send((s.to_string(), msg.to_vec()));
                 }
+
+                self.1.log(format!("Sent notification for topic: {}", s).as_str()).await;
             },
             None => ()
         }
